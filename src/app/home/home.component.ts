8d2e303b1db92f66ca8c59f0518fe748244ab98c
Do Vu Linh <linhdovu95@gmail.com>
2023-02-23 17:02:51 +0700

    FE New

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import {ProductListPageService} from "../service/product-list-page.service";
-// import {CategoryService} from "../service/category.service";
-// import {ActivatedRoute} from "@angular/router";
-// import {RoleService} from "../service/role.service";
-// import {UserService} from "../service/user.service";
-// import {NavService} from "../service/nav.service";
-// @Component({
-//   selector: 'app-home',
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.css']
-// })
-// export class HomeComponent implements OnInit {
-//   constructor(
-//     public pageService: ProductListPageService,
-//     public categoryService: CategoryService,
-//     private activeRouter: ActivatedRoute,
-//     public roleService : RoleService,
-//     public userService : UserService,
-//     public navSerive : NavService
-//   ) { }
-//   //Slider settings
-//   slideConfig = {"slidesToShow": 1, "slidesToScroll": 1} ;
-//
-//
-//   ngOnInit(): void {
-//     this.pageService.getListProduct(0);
-//     this.categoryService.getAllCategory();
-//     this.navSerive.choice = 0
-//     this.roleService.getRoleByParam()
-//   }
-// }
-
 
 import {Component, OnInit} from '@angular/core';
 import {ProductListPageService} from "../service/product-list-page.service";
